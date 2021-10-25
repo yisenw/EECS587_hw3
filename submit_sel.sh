1db3fdb3445fbbbec050b5a6f9daa01eff674b84
yisenw <yisenw@umich.edu>
2021-10-25 17:44:57 -0400

    mihuo

--- submit_sel.sh
+++ submit_sel.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks-per-node=36
 #SBATCH --exclusive
 #SBATCH --mem-per-cpu=1g
-#SBATCH --time=00:15:00
+#SBATCH --time=00:05:00
 #SBATCH --account=eecs587f21_class
 #SBATCH --partition=standard
 
